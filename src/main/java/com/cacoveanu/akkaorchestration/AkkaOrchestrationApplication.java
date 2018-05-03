7a414bb6b0f77486d5b09a5bd70d661ac3b80278
Silviu Cacoveanu <silviu.cacoveanu@gmail.com>
2018-05-03 21:09:22 +0300

    new project

---
+++ src/main/java/com/cacoveanu/akkaorchestration/AkkaOrchestrationApplication.java
@@ -0,0 +1,12 @@
+package com.cacoveanu.akkaorchestration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkkaOrchestrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkkaOrchestrationApplication.class, args);
+	}
+}
