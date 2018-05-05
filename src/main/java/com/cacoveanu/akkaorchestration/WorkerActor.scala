de291a2ce12677a0385fae4176dca7e34ff0e4b1
Silviu Cacoveanu <silviu.cacoveanu@gmail.com>
2018-05-05 20:26:17 +0300

    worker actor with timeout message

--- src/main/java/com/cacoveanu/akkaorchestration/WorkerActor.scala
+++ src/main/java/com/cacoveanu/akkaorchestration/WorkerActor.scala
@@ -6,18 +6,21 @@ import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.{Autowired, Qualifier}
 import org.springframework.context.annotation.Scope
 import org.springframework.stereotype.Component
+import scala.concurrent.duration._
 
 import scala.beans.BeanProperty
 
 object WorkerActor {
   case class Process(id: String)
   case class ProcessResult(success: Boolean)
+  case object Timeout
 }
 
 @Component("workerActorPrototype")
 @Scope("prototype")
 class WorkerActor extends Actor {
   import WorkerActor._
+  import context._
 
   val logger = LoggerFactory.getLogger(classOf[WorkerActor])
 
@@ -39,12 +42,19 @@ class WorkerActor extends Actor {
       this.id = id
       this.initiator = sender()
       dataSourceActor ! Load(id)
+      context.system.scheduler.scheduleOnce(30 seconds, self, Timeout)
+
 
     case LoadResult(data) =>
       logger.info(s"received data for $id")
       // processing this data somehow
       slowService.execute()
       initiator ! ProcessResult(true)
+      self ! PoisonPill
+
+    case Timeout =>
+      logger.info("waiting for data timed out, stopping worker")
+      initiator ! ProcessResult(false)
       self ! PoisonPill
   }
 }
